f724a02500fd47879fc034145bc4ad4e310a42df
Ida Darmochwał <ida.mdart@gmail.com>
2021-05-11 20:48:23 +0200

    Add content to subpage Booking

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,7 @@
 import {settings, select, classNames} from './settings.js'; // {} are used when we import more than one thing and it's NOT default
 import Product from './components/Product.js'; // in Product.js we exported class Product as default, so it can be imported here without {}
 import Cart from './components/Cart.js';
+import Booking from './components/Booking.js';
 
 const app = {
   initPages: function(){
@@ -103,6 +104,15 @@ const app = {
     });
   },
 
+  initBooking: function(){
+    const thisApp = this;
+
+    const bookingContainer = document.querySelector(select.containerOf.booking);
+
+    thisApp.booking = new Booking(bookingContainer);
+
+  },
+
   init: function(){
     const thisApp = this;
     // console.log('*** App starting ***');
@@ -114,6 +124,7 @@ const app = {
     thisApp.initPages();
     thisApp.initData();
     thisApp.initCart();
+    thisApp.initBooking();
   },
 };
 
---
+++ src/js/components/Booking.js
@@ -0,0 +1,45 @@
+import {select, templates} from '../settings.js';
+import AmountWidget from './AmountWidget.js';
+
+class Booking{
+  constructor(element){
+    const thisBooking = this;
+
+    thisBooking.render(element);
+    thisBooking.initWidgets();
+  }
+  render(element){
+    const thisBooking = this;
+    
+    /* generate HTML based on template */
+    const generatedHTML = templates.bookingWidget(thisBooking);
+
+    thisBooking.dom = {};
+
+    thisBooking.dom.wrapper = element;
+    thisBooking.dom.wrapper.innerHTML = generatedHTML;
+    thisBooking.dom.peopleAmount = thisBooking.dom.wrapper.querySelector(select.booking.peopleAmount);
+    thisBooking.dom.hoursAmount = thisBooking.dom.wrapper.querySelector(select.booking.hoursAmount);
+
+  }
+  initWidgets(){
+    const thisBooking = this;
+
+    thisBooking.peopleAmountWidget = new AmountWidget(thisBooking.dom.peopleAmount);
+    thisBooking.hoursAmountWidget = new AmountWidget(thisBooking.dom.hoursAmount);
+
+    thisBooking.dom.peopleAmount.addEventListener('click', function(event){
+      event.preventDefault();
+
+    });
+
+    thisBooking.dom.hoursAmount.addEventListener('click', function(event){
+      event.preventDefault();
+      
+    });
+
+
+  }
+}
+
+export default Booking;
