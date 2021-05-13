d330f7c0b5089c59dbdf54abbed3e551e9254853
Ida Darmochwał <ida.mdart@gmail.com>
2021-05-13 13:44:27 +0200

    Add base class for widgets

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -20,6 +20,8 @@ class Booking{
     thisBooking.dom.wrapper.innerHTML = generatedHTML;
     thisBooking.dom.peopleAmount = thisBooking.dom.wrapper.querySelector(select.booking.peopleAmount);
     thisBooking.dom.hoursAmount = thisBooking.dom.wrapper.querySelector(select.booking.hoursAmount);
+    thisBooking.dom.datePicker = thisBooking.dom.wrapper.querySelector(select.widgets.datePicker.wrapper);
+    thisBooking.dom.hourPicker = thisBooking.dom.wrapper.querySelector(select.widgets.hourPicker.wrapper);
 
   }
   initWidgets(){
