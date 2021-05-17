1028f1180b130246366f1faafd446f6f4ca56012
Ida Darmochwał <ida.mdart@gmail.com>
2021-05-17 14:18:20 +0200

    Show booked & free tables on the booking page

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -1,4 +1,4 @@
-import {select, templates, settings} from '../settings.js';
+import {select, templates, settings, classNames} from '../settings.js';
 import utils from '../utils.js';
 import AmountWidget from './AmountWidget.js';
 import DatePicker from './DatePicker.js';
@@ -22,11 +22,13 @@ class Booking{
 
     thisBooking.dom.wrapper = element;
     thisBooking.dom.wrapper.innerHTML = generatedHTML;
+
     thisBooking.dom.peopleAmount = thisBooking.dom.wrapper.querySelector(select.booking.peopleAmount);
     thisBooking.dom.hoursAmount = thisBooking.dom.wrapper.querySelector(select.booking.hoursAmount);
+    thisBooking.dom.tables = thisBooking.dom.wrapper.querySelectorAll(select.booking.tables);
+
     thisBooking.dom.datePicker = thisBooking.dom.wrapper.querySelector(select.widgets.datePicker.wrapper);
     thisBooking.dom.hourPicker = thisBooking.dom.wrapper.querySelector(select.widgets.hourPicker.wrapper);
-
   }
   initWidgets(){
     const thisBooking = this;
@@ -46,6 +48,9 @@ class Booking{
       
     });
 
+    thisBooking.dom.wrapper.addEventListener('updated', function(){ // custom event from class BaseWidget
+      thisBooking.updateDOM();
+    });
 
   }
   getData(){
@@ -57,12 +62,12 @@ class Booking{
 
     const params = {
       bookings: [
-        //startDateParam,
+        startDateParam,
         endDateParam,
       ],
       eventsCurrent: [
         settings.db.notRepeatParam,
-        //startDateParam,
+        startDateParam,
         endDateParam,
       ],
       eventsRepeat: [
@@ -71,7 +76,7 @@ class Booking{
       ],
     };
 
-    console.log(params);
+    // console.log(params);
 
     const urls = {
       bookings: settings.db.url + '/' + settings.db.bookings + '?' + params.bookings.join('&'),
@@ -79,7 +84,7 @@ class Booking{
       eventsRepeat: settings.db.url + '/' + settings.db.events + '?' + params.eventsRepeat.join('&'),
     };
 
-    console.log(urls);
+    // console.log(urls);
 
     Promise.all([ // works similar to method fetch. Promise.all() takes an array of promises as an input parameter and returns a new Promise object that will fulfil only if and when all promises in the array fulfil.
       fetch(urls.bookings),
@@ -97,10 +102,93 @@ class Booking{
         ]);
       })
       .then(function([bookings, eventsCurrent, eventsRepeat]){ // gets converted objects
-        console.log(`b`, bookings);
-        console.log(`eC`, eventsCurrent);
-        console.log(`eR`, eventsRepeat);
+        // console.log(`b`, bookings);
+        // console.log(`eC`, eventsCurrent);
+        // console.log(`eR`, eventsRepeat);
+        thisBooking.parseData(bookings, eventsCurrent, eventsRepeat);
       });
+  }
+
+  parseData(bookings, eventsCurrent, eventsRepeat){
+    const thisBooking = this;
+
+    thisBooking.booked = {};
+
+    for(let item of bookings){
+      thisBooking.makeBooked(item.date, item.hour, item.duration, item.table);
+    }
+
+    for(let item of eventsCurrent){
+      thisBooking.makeBooked(item.date, item.hour, item.duration, item.table);
+    }
+
+    const minDate = thisBooking.datePicker.minDate;
+    const maxDate = thisBooking.datePicker.maxDate;
+
+    for(let item of eventsRepeat){
+      if(item.repeat === 'daily'){
+        for(let loopDate = minDate; loopDate <= maxDate; loopDate = utils.addDays(loopDate, 1)){ // loopDate++ wouldn't work here, 'cause dates work differently than numbers, therefore utils.addDate is needed
+          thisBooking.makeBooked(utils.dateToStr(loopDate), item.hour, item.duration, item.table);
+        
+        } 
+      }
+    }
+
+    //console.log(`thisBooking.booked`, thisBooking.booked);
+
+    thisBooking.updateDOM(); // this method should be executed right after we get data from API, so in method parseData
+  }
+  makeBooked(date, hour, duration, table){
+    const thisBooking = this;
+
+    if(typeof thisBooking.booked[date] == 'undefined'){ // is needed for pushing table to thisBooking.booked
+      thisBooking.booked[date] = {};
+    }
+
+    const startHour = utils.hourToNumber(hour);
+
+    for(let hourBlock = startHour; hourBlock < (startHour + duration); hourBlock += 0.5){ // 0.5 is put due to the fact that we set the shortest reseravtion to last 30min.
+      //console.log('loop', hourBlock);
+
+      if(typeof thisBooking.booked[date][hourBlock] == 'undefined'){ // is needed for pushing table to thisBooking.booked
+        thisBooking.booked[date][hourBlock] = [];
+      }
+  
+      thisBooking.booked[date][hourBlock].push(table); // in object thisBooking.booked we find object [date] given as first arg of method makeBooked. In [data] object we find a property key ([hour]) equaled to the second arg of the method. [hour] is an array
+    }
+  }
+  updateDOM(){
+    const thisBooking = this;
+
+    thisBooking.date = thisBooking.datePicker.value;
+    thisBooking.hour = utils.hourToNumber(thisBooking.hourPicker.value);
+
+    let allAvailable = false; // will mean that this day at this time all tables are available
+
+    if(
+      typeof thisBooking.booked[thisBooking.date] === 'undefined' // there is no object created for this date
+      ||
+      typeof thisBooking.booked[thisBooking.date][thisBooking.hour] === 'undefined' // there is no array cretaed for this date at this time
+    ){
+      allAvailable = true;
+    }
+
+    for(let table of thisBooking.dom.tables){
+      let tableId = table.getAttribute(settings.booking.tableIdAttribute); // tableId is always a string but it can be converted into number using parseInt (below)
+      if(!isNaN(tableId)){
+        tableId = parseInt(tableId);
+      }
+
+      if(
+        !allAvailable
+        &&
+        thisBooking.booked[thisBooking.date][thisBooking.hour].includes(tableId)
+      ){
+        table.classList.add(classNames.booking.tableBooked);
+      } else {
+        table.classList.remove(classNames.booking.tableBooked);
+      }
+    }
   }
 }
 
